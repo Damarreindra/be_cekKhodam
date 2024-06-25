cee6752c616bea8a136e97624129b81d7da840d0
Damarreindra <damarihya08@gmail.com>
2024-06-26 00:37:47 +0700

    try

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const Khodam = require("./models/Khodam");
+
 mongoose
 .connect("mongodb://127.0.0.1/shop_db")
 .then((result) => {
@@ -12,6 +14,16 @@ mongoose
   console.log(err);
 });
 
+app.get('/khodam', async (req, res) => {
+  try {
+    const count = await Khodam.countDocuments();
+    const randomIndex = Math.floor(Math.random() * count);
+    const randomKhodam = await Khodam.findOne().skip(randomIndex);
+    res.send(randomKhodam);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 app.listen(PORT, () => {
     console.log(`Server berjalan di http://localhost:${PORT}`);
