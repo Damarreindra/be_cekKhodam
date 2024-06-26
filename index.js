345ed3180c41615f01178a8a6bc30d0eaaa125a6
Damarreindra <damarihya08@gmail.com>
2024-06-26 22:00:16 +0700

    fix mongodb

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 const Khodam = require("./models/Khodam");
 
 mongoose
-.connect("mongodb://127.0.0.1/dbKhodam")
+.connect("mongodb+srv://damar:Luhan1603@cluster0.todawlr.mongodb.net/dbKhodam?retryWrites=true&w=majority")
 .then((result) => {
   console.log("connected to mongodb");
 })
@@ -28,7 +28,6 @@ app.get('/khodam', async (req, res) => {
       return res.status(404).json({ message: 'No Khodam found' });
     }
 
-    console.log(randomKhodam);
     res.send(randomKhodam);
   } catch (error) {
     res.status(500).json({ message: error.message });
