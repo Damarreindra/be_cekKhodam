db3db50897fef3be6fc2305419ec7544c362f1eb
Damarreindra <damarihya08@gmail.com>
2024-06-27 22:49:18 +0700

    fix mongodb

--- index.js
+++ index.js
@@ -29,32 +29,37 @@ const startServer = async () => {
       fn(req, res, next).catch((err) => next(err));
     };
   }
- 
   app.get('/khodam', wrapAsync(async (req, res) => {
-    const { name } = req.query;
-    console.log('Received name:', name);
-
+    const { name } = req.query; 
     try {
-        if (name === "rehan") {
-            res.json({ name, khodam: "Anjing Rabies" });
-        } else if (name === "dile") {
-            res.json({ name, khodam: "Antena Biru" });
-        } else {
-            const count = await Khodam.countDocuments();
-            if (count === 0) {
-                return res.status(404).json({ message: 'No Khodam found' });
-            }
-
-            const randomIndex = Math.floor(Math.random() * count);
-            const randomKhodam = await Khodam.findOne().skip(randomIndex);
-
-            if (!randomKhodam) {
-                return res.status(404).json({ message: 'No Khodam found' });
-            }
-
-            res.json({ name, khodam: randomKhodam.name });
+        const count = await Khodam.countDocuments();
+        console.log('Document count:', count); // Log document count
+        if (count === 0) {
+            return res.status(404).json({ message: 'No Khodam found' });
         }
+
+        const randomIndex = Math.floor(Math.random() * count);
+        const randomKhodam = await Khodam.findOne().skip(randomIndex);
+        if (!randomKhodam) {
+            return res.status(404).json({ message: 'No Khodam found' });
+        }
+        if(name==="rehan"){
+          res.json({ name, khodam: {name:"Anjing Rabies"} });
+        }else if(name==="dile"){
+          res.json({ name, khodam: {name:"Antena Biru"} });
+          
+        }else if(name==="raihan"){
+          res.json({ name, khodam: {name:"Badut"} });
+        }
+        else if(name==="alpat"){
+          res.json({ name, khodam: {name:"Starlght Princess", img:'https://res.cloudinary.com/dttd52ltg/image/upload/v1719501663/gbvqydur5fe1jhan5r4c.jpg'} });
+        }
+        else{
+          res.json({ name, khodam: randomKhodam });
+        }
+      
     } catch (error) {
+        console.error('Error fetching Khodam:', error.message); 
         res.status(500).json({ message: error.message });
     }
 }));
